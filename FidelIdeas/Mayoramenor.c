1a9414c77921178d319a8ac277d742ffc0b6d720
Fidek17 <fgilbertodv@gmail.com>
2023-05-03 15:49:59 -0600

    Ya logra ordenar numeros random dados sin la ayuda de frida

--- FidelIdeas/Mayoramenor.c
+++ FidelIdeas/Mayoramenor.c
@@ -1,28 +1,57 @@
 # include <stdio.h>
+#include <stdlib.h>
+struct Lugar{
+    int NumRand, clave;
+
+}; 
+
+int generardatos(struct Lugar *A, int n);
+
 
 int main(){
-    int k1, k2, aux, n, lista[10]; 
-    n = 5;
+    struct Lugar *ayuda;
+    ayuda=(struct Lugar *) malloc(16 * sizeof(struct Lugar)); 
+
+
+
+    int k1, k2, aux, n, lista[20]; 
+    n = 16;
+
+    generardatos(ayuda, n);
+
     for(k1=0; k1<n; k1++){
-        printf("Dame el dato %d:  ", k1+1);
-        scanf("%d", &lista[k1]);
+        printf("El dato generado %d es : %d ", ayuda[k1].clave, ayuda[k1].NumRand);
     }
+    
 
     for(k1=0; k1<n; k1++){
         for(k2=0; k2<n; k2++){
-            if(lista[k1]> lista[k2]){
-                aux= lista[k1];
-                lista[k1]= lista[k2];
-                lista[k2]= aux;
+            if(ayuda[k1].NumRand> ayuda[k2].NumRand){
+                aux= ayuda[k1].NumRand;
+                ayuda[k1].NumRand= ayuda[k2].NumRand;
+                ayuda[k2].NumRand= aux;
             }
 
         }     
     }
 
-    printf("\n\nLos datos ordenados son: ");
-    for(k1=0; k1<n; k1++){
-        printf("%d, ", lista[k1]);
-        
+ for(k1=0; k1<n; k1++){
+        printf("Los dato ordenados son: \n\n");
+        printf("%d", ayuda[k1].NumRand);
+    }
+    
+
+
+    return 0; 
+}
+
+int generardatos(struct Lugar *A, int n){
+    int i;
+    for(i=0; i < n; i++){
+        A[i].clave = i; 
+
+        A[i].NumRand = rand()%200; 
+
     }
 
     return 0; 
