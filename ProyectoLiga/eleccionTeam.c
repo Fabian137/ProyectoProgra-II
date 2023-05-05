bac0b6b535c7ba45a9e0c32d37216e8f855ed3f3
Fabian137 <c.fabian.bel@gmail.com>
2023-05-04 23:12:49 -0600

    Trabajando en el enfrentamiento de equipos, primera ronda

--- ProyectoLiga/eleccionTeam.c
+++ ProyectoLiga/eleccionTeam.c
@@ -5,52 +5,51 @@
 
 struct futbolTeams{
     char teamsName[20], D_Tecnico[20], Capitan[10], MVP[10];
-    int clave, n_jugadores, goles, faltas, ganadas, perdidas, numRand;
+    int clave, n_jugadores, goles, faltas, ganadas, perdidas, numRand, NumCampeon;
+    /*
+      clave -       para identificar el equipo sin usar el nombre. teamsName
+    ! numRand -     No sé si se sigue necesitando . . .
+    ! perdidas -    Tampoco estoy seguro si se necesite . . .
+      NumCampeon -  Para tener un historial de campeones de la LIGA
+
+    */
 };
 
-void leerDatos(struct futbolTeams *, int);
-void generarDatos(struct futbolTeams *, int, int arr[8]);
+void generarDatos(struct futbolTeams *, int, int *);
 void imprimirDatos(struct futbolTeams *, int);
-void ordenar(struct futbolTeams *, int);
-int busqueda_NRandomEquipo(struct futbolTeams *, int );
 int idenficadorEquipo(struct futbolTeams *, int , int);
 int aleatorio(int min, int max);
-void generararreglo(int numeroequipos, int arr[8]);
+void generararreglo(int numeroequipos, int *arr);
+
+int filas_CSV();
+// void enfrentamientos(struct futbolTeams *,  int);
+void enfrentamientos(struct futbolTeams *);
 
 int main(){
     struct futbolTeams *equipo;
-    int max, numTeam = 0, i, n, indicealeatorio, temporal;
+    int i, indicealeatorio, temporal, n=8;
     equipo = (struct futbolTeams *) malloc(16 * sizeof(struct futbolTeams));
-    n = 8; 
-    int arr[8];
-    // leerDatos(equipo, 3);
+    // n = 8; 
+    // int arr[8];
+    int *arr;
+    arr = (int *) malloc(n * sizeof(int));
 
     generarDatos(equipo, n, arr);
-    
-
+        
     printf("\n\nArreglo ya mezclado: \n\n"); 
         for(i=0; i < n; i++){
             printf("%d\n", arr[i]);
 
     } 
 
-
     imprimirDatos(equipo, n);
+
+    enfrentamientos(equipo);
+/*
+*/
 
     return 0;
 
-}
-
-void leerDatos(struct futbolTeams *F, int n){
-    int i;
-    for (i = 0; i < n; i++){
-        printf("\nIngresa el nombre del equipo ");
-        scanf("%s", F[i].teamsName);
-        printf("\nIngresa el nombre del Director Tecnico ");
-        scanf("%s", F[i].D_Tecnico);
-        printf("\nIngresa el nombre del Capitan ");
-        scanf("%s", F[i].Capitan);
-    }
 }
 
 void generarDatos(struct futbolTeams *F, int n, int arr[8]){
@@ -64,10 +63,8 @@ void generarDatos(struct futbolTeams *F, int n, int arr[8]){
     if (archivo == NULL) {
         printf("Error al abrir el archivo\n");
     }
-    /*
-    */
-    //srand(time(NULL));
-            srand(time(NULL));
+
+    srand(time(NULL));
 
 //-------------Fragmento que mezcla los datos ------------------------------
     generararreglo(n, arr);
@@ -84,10 +81,7 @@ void generarDatos(struct futbolTeams *F, int n, int arr[8]){
         
         F[j].n_jugadores = 11;
         F[j].goles = rand() % 5;
-        // F[i].ganadas = rand() % 4;
-        // F[i].perdidas = rand() % 4;
         F[j].faltas = rand() % 4;
-        // F[i].clave = i +100;
         F[j].clave = j;
     }
 
@@ -97,7 +91,9 @@ void generarDatos(struct futbolTeams *F, int n, int arr[8]){
 
 void imprimirDatos(struct futbolTeams *F, int n){
     int i;
+    FILE *archivo_Infos;
     FILE *archivo_ids;
+    archivo_Infos = fopen("ID_teams.txt", "w");
     archivo_ids = fopen("ID_teams.csv", "w");
 
     for ( i = 0; i < n; i++){
@@ -112,15 +108,15 @@ void imprimirDatos(struct futbolTeams *F, int n){
         printf("\n\t\tPerdidos: %d ----- Ganados: %d", F[i].perdidas, F[i].ganadas);
         printf("\n\t\tGoles: %d ----- Faltas: %d \n", F[i].goles, F[i].faltas);
     
-        fprintf(archivo_ids, "%d, %d, %s\n", F[i].clave, i, F[i].teamsName);
+        fprintf(archivo_Infos, "%d, %d, %s  ------ G: %d -------- F: %d\n", F[i].clave, i, F[i].teamsName, F[i].goles, F[i].faltas);
+        fprintf(archivo_ids, "%d, \n", F[i].clave, i, F[i].teamsName, F[i].goles, F[i].faltas);
     }
+    fclose(archivo_Infos);
     fclose(archivo_ids);
 }
 
 int aleatorio(int min, int max){
-
-return min + rand()/ (RAND_MAX/ (max - min + 1 ) + 1);
-
+    return min + rand()/ (RAND_MAX/ (max - min + 1 ) + 1);
 }
 
 void generararreglo(int n, int arr[8]){
@@ -131,7 +127,7 @@ void generararreglo(int n, int arr[8]){
     
     for(i=0; i < n; i++){
         int temporal; 
-        int indicealeatorio = aleatorio (0, n-1); 
+        int indicealeatorio = aleatorio(0, n-1); 
 
         temporal = arr[i];
         arr[i] = arr[indicealeatorio];
@@ -139,4 +135,43 @@ void generararreglo(int n, int arr[8]){
 
     }
     
+}
+
+int filas_CSV(){
+    FILE *archivoIDS;
+    archivoIDS = fopen("ID_teams.csv", "r");
+    int v, i=0;
+    while (fscanf(archivoIDS, "%d,", v) != EOF){
+        i++;
+    }
+    return i;
+    
+}
+
+void enfrentamientos(struct futbolTeams *F){
+    FILE *archivoIDS;
+
+    archivoIDS = fopen("ID_teams.csv", "r");
+    int i, id_team, n;
+    n = filas_CSV();
+    printf("%d", n);
+
+    /*
+    while (fscanf(archivoIDS, "%d,", id_team) != EOF){
+        i++;
+        if(){
+        
+        }
+        
+    }
+    */
+    
+    for (i = 0; i < n; i+=2){
+        if(fscanf(archivoIDS, "%d,", id_team) != EOF){
+            if(){
+
+            }
+        }
+        printf("%d -- %d\n", i, i+1);
+    }
 }
